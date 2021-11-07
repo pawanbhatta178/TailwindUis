b8556ff20d7d45a47ff7544e088196ded65efc5b
pawan <pawanbhatta178@gmail.com>
2021-11-07 12:11:04 -0500

    .

--- src/components/Listbox/Listbox.tsx
+++ src/components/Listbox/Listbox.tsx
@@ -1,59 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Select, { components } from "react-select";
 import { AiFillCaretDown } from "react-icons/ai";
 
-
 const DropdownIndicator = (props: any) => {
-    return (
-        <components.DropdownIndicator {...props}>
-            <AiFillCaretDown />
-        </components.DropdownIndicator>
-    );
+  return (
+    <components.DropdownIndicator {...props}>
+      <AiFillCaretDown />
+    </components.DropdownIndicator>
+  );
 };
 
-
 interface IListboxProps extends IBaseProps {
-    options: any;
-    isMulti?: boolean | undefined;
-    isSearchable?: boolean | undefined;
-    isLoading?: boolean | undefined;
-    isDisabled?: boolean | undefined;
-    defaultValue?: any;
-    placeholder?: string;
+  options: any;
+  isMulti?: boolean | undefined;
+  isSearchable?: boolean | undefined;
+  isLoading?: boolean | undefined;
+  isDisabled?: boolean | undefined;
+  defaultValue?: any;
+  placeholder?: string;
 }
 
+const Listbox: React.FC<IListboxProps> = ({
+  options,
+  isMulti,
+  isDisabled,
+  isLoading,
+  isSearchable,
+  defaultValue,
+  placeholder,
+}) => {
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      "&:active": { borderColor: "gray" },
+      "&:focus": { borderColor: "gray" },
+      "&:select": { borderColor: "gray" },
+      "&:hover": { borderColor: "gray" },
+      borderColor: "none",
+      boxShadow: "none",
+      backgroundColor: "#F4F4F5",
+    }),
+    option: (
+      provided: any,
+      { data, isDisabled, isFocused, isSelected }: any
+    ) => ({
+      ...provided,
+      backgroundColor: isSelected ? "#9333EA" : isFocused ? "#F3E8FF" : null,
+    }),
+    indicatorSeparator: (provided: any) => ({
+      ...provided,
+      display: "none",
+    }),
+  };
 
-const Listbox: React.FC<IListboxProps> = ({ options, isMulti, isDisabled, isLoading, isSearchable, defaultValue, placeholder }) => {
+  const [region, setRegion] = useState(defaultValue);
 
-    const customStyles = {
-        control: (provided: any) => ({
-            ...provided,
-            '&:active': { borderColor: 'gray' },
-            '&:focus': { borderColor: 'gray' },
-            '&:select': { borderColor: 'gray' },
-            '&:hover': { borderColor: 'gray' },
-            borderColor: 'none',
-            boxShadow: "none",
-            backgroundColor: "#F4F4F5"
-        }),
-        option: (provided: any, { data, isDisabled, isFocused, isSelected }: any) => ({
-            ...provided,
-            backgroundColor: isSelected ? "#9333EA" : (isFocused ? "#F3E8FF" : null),
-        }),
-        indicatorSeparator: (provided: any) => ({
-            ...provided,
-            display: "none"
-        })
-    }
+  const onchangeSelect = (item: any) => {
+    setRegion(item);
+  };
 
-    const [region, setRegion] = useState(defaultValue);
-
-    const onchangeSelect = (item: any) => {
-        setRegion(item);
-    };
-
-    return (
-        <Select
+  return (
+    <>
+      <Select
         isMulti={isMulti}
         isSearchable={isSearchable}
         isDisabled={isDisabled}
@@ -66,13 +74,9 @@ const Listbox: React.FC<IListboxProps> = ({ options, isMulti, isDisabled, isLoad
         components={{ DropdownIndicator }}
         getOptionValue={(option: any) => option.value}
         getOptionLabel={(option: any) => option.value}
-    />
-       
-    )
-}
+      />
+    </>
+  );
+};
 
-
-
-
-
-export { Listbox }
+export { Listbox };
