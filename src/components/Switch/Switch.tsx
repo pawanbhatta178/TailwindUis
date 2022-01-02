910fe763d474e7fe289255c702ebe1b58d0ec686
pawan <pawanbhatta178@gmail.com>
2022-01-01 21:25:37 -0500

    .

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,31 +1,36 @@
-import { Switch as NSwitch } from '@headlessui/react'
+import { Switch as NSwitch } from "@headlessui/react";
 
 interface ISwitchProps extends IBaseProps {
-    label?: string | React.ReactNode;
-    enabled: boolean;
-    onChange: () => void;
+  label?: string | React.ReactNode;
+  enabled: boolean;
+  onChange: () => void;
 }
 
-
-export const Switch: React.FC<ISwitchProps> = ({ label, enabled, onChange }) => {
-
-
-    return (
-        <NSwitch.Group>
-            <div className="flex items-center justify-between ">
-                {label && <NSwitch.Label >{label}</NSwitch.Label>}
-                <NSwitch
-                    checked={enabled}
-                    onChange={onChange}
-                    className={`${enabled ? 'bg-purple-600' : 'bg-gray-200'
-                        } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
-                >
-                    <span
-                        className={`${enabled ? 'translate-x-6' : 'translate-x-1'
-                            } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
-                    />
-                </NSwitch>
-            </div>
-        </NSwitch.Group>
-    )
-}
+export const Switch: React.FC<ISwitchProps> = ({
+  label,
+  enabled,
+  onChange,
+}) => {
+  return (
+    <>
+      <NSwitch.Group>
+        <div className="flex items-center justify-between ">
+          {label && <NSwitch.Label>{label}</NSwitch.Label>}
+          <NSwitch
+            checked={enabled}
+            onChange={onChange}
+            className={`${
+              enabled ? "bg-purple-600" : "bg-gray-200"
+            } relative inline-flex items-center h-6 rounded-full w-11 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
+          >
+            <span
+              className={`${
+                enabled ? "translate-x-6" : "translate-x-1"
+              } inline-block w-4 h-4 transform bg-white rounded-full transition-transform`}
+            />
+          </NSwitch>
+        </div>
+      </NSwitch.Group>
+    </>
+  );
+};
